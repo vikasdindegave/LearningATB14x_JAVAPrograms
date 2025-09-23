9d8bbf15879193871f1885c94a4c6419a7f53834
win 10 <dindegavevikas@gmail.com>
2025-09-24 00:44:52 +0530

    practice basics

---
+++ src/ex_01_java_basics/Lab013_PrimeNumber.java
@@ -0,0 +1,30 @@
+package ex_01_java_basics;
+
+import java.util.Scanner;
+
+public class Lab013_PrimeNumber {
+    public static void main(String[] args) {
+        System.out.println("Enter a number");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        int m = num /2;
+        int flag = 0;
+
+        if(num==0 || num ==1){
+            System.out.println(num+" is not a prime number");
+        }else{
+            for(int i=2; i<=m; i++){
+                if(num%i==0) {
+                    System.out.println(num+" is not a prime number");
+                    flag = 1;
+                    break;
+                }
+            }
+
+            if(flag==0) {
+                System.out.println(num + " is a prime number");
+            }
+        }
+    }
+}
